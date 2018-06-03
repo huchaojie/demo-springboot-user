6180246a28b3d620754bb85297348ffd17eb8028
huchaojie <15173488874@163.com>
2018-06-03 10:14:42 +0800

    Initial commit

---
+++ src/main/java/com/yc/demospringbootuser/DemoSpringbootUserApplication.java
@@ -0,0 +1,12 @@
+package com.yc.demospringbootuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbootUserApplication.class, args);
+	}
+}
