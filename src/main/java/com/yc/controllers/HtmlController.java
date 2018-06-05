f6d21c29dc33bfd9df593937c2bdf1b73c0f84c0
huchaojie <15173488874@163.com>
2018-06-05 10:57:38 +0800

    6/05 胡超结

--- src/main/java/com/yc/controllers/HtmlController.java
+++ src/main/java/com/yc/controllers/HtmlController.java
@@ -1,11 +1,19 @@
 package com.yc.controllers;
 
 import com.sun.deploy.net.HttpResponse;
+import com.yc.entity.Students;
+import com.yc.service.StudentsService;
+import com.yc.util.JsonMode;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Author: 胡超结
@@ -13,16 +21,38 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 
 @Api("页面跳转控制器")
-@RequestMapping("/")
+@RequestMapping(value = "/")
 @Controller
 public class HtmlController {
 
+
+    @Resource(name="studentsServiceImpl")
+    private StudentsService studentsService;
+
+    @ApiOperation(value = "进入注册页面 跳转到reg.html")
     @RequestMapping(value = "reg.action")
     public String reg(){
-
         return "reg";
     }
 
+    @ApiOperation(value = "进入登录页面 跳转到login.html")
+    @RequestMapping(value = "login.action")
+    public String moba(){
+        return "According";
+    }
+
+    @ApiOperation(value = "进入登录页面 跳转到login.html")
+    @RequestMapping(value = "a.action")
+    @ResponseBody
+    public JsonMode login(JsonMode jsonMode){
+        Students st=new Students();
+        List<Students> list=new ArrayList<Students>();
+        list=studentsService.functionAll(st);
+        jsonMode.setObj(list);
+        jsonMode.setCode(1);
+
+        return jsonMode;
+    }
 
 }
 
--- src/main/resources/mydoc/demo-springboot-user.sql
+++ src/main/resources/mydoc/demo-springboot-user.sql
@@ -9,3 +9,16 @@ CREATE TABLE `students` (
   `gender` varchar(4) DEFAULT NULL COMMENT '性别',
   PRIMARY KEY (`sid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户Id',
+  `name` varchar(255) NOT NULL COMMENT '用户名',
+  `password` varchar(255) NOT NULL COMMENT '登录密码',
+  `mobile_phone` int(11) NOT NULL COMMENT '手机号码',
+  `age` int(11) NOT NULL COMMENT '年龄',
+  `sex` varchar(255) NOT NULL COMMENT '性别',
+  `email` varchar(255) NOT NULL COMMENT '电子邮件',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
